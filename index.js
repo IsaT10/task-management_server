b99e0094a5bfcc8608773044aae46b57395b9a24
IsaT <rakiib010@gmail.com>
2023-12-21 12:24:46 +0600

    server setup with mongodb

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+//taskManagementDB 4jMVDf0HgxDbUjIK
+
+const uri = `${process.env.URI}`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('task management server is running');
+});
+
+app.listen(port, () => {
+  console.log(`task management server is running :${port}`);
+});
